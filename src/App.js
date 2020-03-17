060c41dbdf0110441f4d7bc7c54f41944741eaea
Daniel <daniel.kapexhiu@gmail.com>
2020-03-17 21:53:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
-import dataList from "./dataList.json";
+import axios from "axios";
 
 function App() {
   const [searchText, setSearchText] = useState("");
-  const [data, setData] = useState(dataList);
+  const [data, setData] = useState(null);
   const excludeColumns = [""];
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      "https://json-api-test123.herokuapp.com/data"
+    );
+    setData(response.data);
+  };
 
   // handle change event of search input
   const handleChange = value => {
     setSearchText(value);
     filterData(value);
+    fetchData();
   };
 
   // filter records by search text
@@ -42,25 +50,24 @@ function App() {
         onChange={e => handleChange(e.target.value)}
       />
       <div className="box-container">
-        {data.map((d, i) => {
-          return (
-            <div key={i} className="box">
-              <b>Country: </b>
-              {d.Country_Region}
-              <br />
-              <b>Confirmed: </b>
-              {d.Confirmed}
-              <br />
-              <b>Deaths: </b>
-              {d.Deaths}
-              <br />
-              <b>Recovered: </b>
-              {d.Recovered}
-            </div>
-          );
-        })}
-        <div className="clearboth" />
-        {data.length === 0 && <span>No records found to display!</span>}
+        {data &&
+          data.map((d, i) => {
+            return (
+              <div key={i} className="box">
+                <b>Country: </b>
+                {d.Country_Region}
+                <br />
+                <b>Confirmed: </b>
+                {d.Confirmed}
+                <br />
+                <b>Deaths: </b>
+                {d.Deaths}
+                <br />
+                <b>Recovered: </b>
+                {d.Recovered}
+              </div>
+            );
+          })}
       </div>
     </div>
   );
